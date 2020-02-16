91f896e1dc874b166c257dbd23c095630057d7ba
Ioannis Koutras <ioannis.koutras@gmail.com>
2020-02-16 14:25:33 +0100

    Add library dependencies

--- liststoiptables.nix
+++ liststoiptables.nix
@@ -1,11 +1,15 @@
-{ mkDerivation, base, stdenv }:
+{ mkDerivation, base, bytestring, HTTP, iproute, network-uri
+, stdenv, zlib
+}:
 mkDerivation {
   pname = "ListsToIpTables";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base ];
+  executableHaskellDepends = [
+    base bytestring HTTP iproute network-uri zlib
+  ];
   description = "Download IP lists and transform them to IP sets";
   license = stdenv.lib.licenses.gpl3;
 }
