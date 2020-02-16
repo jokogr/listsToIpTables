50fa0b57b832dd236c719006a2421d891bc25370
Ioannis Koutras <ioannis.koutras@gmail.com>
2020-02-16 14:05:49 +0100

    shell.nix: Use liststoiptables.nix

--- shell.nix
+++ shell.nix
@@ -4,17 +4,7 @@ let
 
   inherit (nixpkgs) pkgs;
 
-  f = { mkDerivation, base, stdenv }:
-      mkDerivation {
-        pname = "ListsToIpTables";
-        version = "0.1.0.0";
-        src = ./.;
-        isLibrary = false;
-        isExecutable = true;
-        executableHaskellDepends = [ base ];
-        description = "Download IP lists and transform them to IP sets";
-        license = stdenv.lib.licenses.gpl3;
-      };
+  f = import ./liststoiptables.nix;
 
   haskellPackages = if compiler == "default"
                        then pkgs.haskellPackages
